960e82f8fca42248bff89f93e0285dfeae8ca8cb
Gabriel <gab.matos@outlook.com>
2021-10-14 12:07:02 +0100

    Created initial attributes, constructor and methods for main class

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -1,4 +1,26 @@
 package com.company;
 public abstract class Library {
 
+    ////////////////Attributes/////////////////
+    private String name;
+
+    ////////////////Constructor////////////////
+    public Library(String name) {
+        this.name = name;
+    }
+
+    ////////////////Methods///////////////
+    public void book(){
+        System.out.println("I am a book");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
